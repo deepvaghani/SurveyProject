6abe41548917ff50c7ca6b2c058c74697e7d3968
Deep Vaghani <20it155@charusat.edu.in>
2023-06-17 16:21:28 +0530

    Updates

---
+++ precache-manifest.1674cad37374ab12d824ece623acc75e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "36ef4b005123409898f4",
+    "url": "/static/css/main.314b776f.chunk.css"
+  },
+  {
+    "revision": "36ef4b005123409898f4",
+    "url": "/static/js/main.0b3ff769.chunk.js"
+  },
+  {
+    "revision": "716c6dee587b2db6c9a5",
+    "url": "/static/js/runtime~main.4a856da1.js"
+  },
+  {
+    "revision": "e1b944b85d912d879142",
+    "url": "/static/css/2.5f32a4e1.chunk.css"
+  },
+  {
+    "revision": "e1b944b85d912d879142",
+    "url": "/static/js/2.6d0ec6cc.chunk.js"
+  },
+  {
+    "revision": "326ba02ee63f8ce55f86",
+    "url": "/static/js/3.82729454.chunk.js"
+  },
+  {
+    "revision": "d4723e891ae1839694f36912670f2672",
+    "url": "/index.html"
+  }
+];
