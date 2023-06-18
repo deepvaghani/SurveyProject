9f8b28056ab737e80e93a05271fd89015e4bb9f7
Deep Vaghani <20it155@charusat.edu.in>
2023-06-18 13:06:35 +0530

    Commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const User = require('./User');
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
 app.use(bodyParser.json());
@@ -15,7 +16,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // MongoDB Atlas connection string
-const uri = 'mongodb+srv://deepvaghani58:Deep2023@surveydb.3s9yqz7.mongodb.net/surveyCollection?retryWrites=true&w=majority';
+const uri = process.env.MONGODB_URL;;
 
 // Connect to MongoDB Atlas
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
