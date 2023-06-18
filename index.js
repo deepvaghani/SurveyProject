307c9899b3a3097655cf8ef6b70386ba73155c1b
Deep Vaghani <20it155@charusat.edu.in>
2023-06-18 14:11:54 +0530

    Commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const corsOptions = {
 // Add a middleware to set the CORS headers
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://techinsights.netlify.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, FETCH');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
